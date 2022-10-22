960857e74016a855d2bd4bc0f6da0c8bf177e260
ThangB1910143 <104030447+NguyenChienThangB1910143@users.noreply.github.com>
2022-10-22 22:42:14 +0700

    Quan ly trang thái voi provider - CartManager

--- myshop/lib/ui/products/product_grid_tile.dart
+++ myshop/lib/ui/products/product_grid_tile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../models/product.dart';
 
+import '../cart/cart_manager.dart';
 import 'products_detail_screen.dart';
 import 'products_detail_screen.dart';
 
@@ -37,7 +39,7 @@ class ProductGridTile extends StatelessWidget {
       backgroundColor: Colors.black87,
       leading: ValueListenableBuilder<bool>(
         valueListenable: product.isFavoriteListenable,
-        builder: (ctx, isFavorite, child){
+        builder: (ctx, isFavorite, child) {
           return IconButton(
             icon: Icon(
               isFavorite ? Icons.favorite : Icons.favorite_border,
@@ -48,7 +50,7 @@ class ProductGridTile extends StatelessWidget {
             },
           );
         },
-      ), 
+      ),
       title: Text(
         product.title,
         textAlign: TextAlign.center,
@@ -58,7 +60,24 @@ class ProductGridTile extends StatelessWidget {
           Icons.shopping_cart,
         ),
         onPressed: () {
-          print('Add item to cart');
+          final cart = context.read<CartManager>();
+          cart.addItem(product);
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: const Text(
+                  'Item added to cart',
+                ),
+                duration: const Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id!);
+                  },
+                ),
+              ),
+            );
         },
         color: Theme.of(context).colorScheme.secondary,
       ),
